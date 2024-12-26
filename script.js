79df3148e61b076b7e22e33bdf9023ea7cc607b4
harii77 <hariharanvj7777@gmail.com>
2024-12-26 19:35:05 +0530

    updated script

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('Mapty // Map your workouts!!');
+// console.log('Mapty // Map your workouts!!');
+
+'use strict';
+
+class Workout {}
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
